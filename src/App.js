c7d819f9b3d36d34816bb8d4cd3743029f5af48e
FerSosa64 <fernandososa03@unitec.edu>
2024-11-21 12:11:11 -0600

    Pagina de ajustes terminada (FRONTEND)
    Se termino la el frontend de la pagina de ajustes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Chat from './pages/Chat';
 import History from './pages/History';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Profileconfig from './pages/Profileconfig';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path='/login' element={<Login/>}/>
           <Route path='/register' element={<Register/>}/>
           <Route path='/home' element={<Home/>}/>
+          <Route path='/adjustments' element={<Profileconfig/>}/>
         </Routes>
       </Router>
     </div>
